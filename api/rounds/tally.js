ff74e5d57f100067f1390d11d243c647c94c4d75
PadhleYrr <moliage490@gmail.com>
2026-06-29 10:55:35 +0530

    unzipgit

--- api/rounds/tally.js
+++ api/rounds/tally.js
@@ -1,10 +1,9 @@
-// api/rounds/tally.js
+"use strict";
 const { getDb } = require("../_db");
 
 module.exports = async (req, res) => {
   if (req.method === "OPTIONS") return res.status(200).end();
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-
   try {
     const db = await getDb();
     const { roundId } = req.body;
@@ -14,15 +13,13 @@ module.exports = async (req, res) => {
 
     const players = await db.collection("players").find({ roomId: round.roomId }).toArray();
 
-    // Tally votes
+    // Count votes
     const counts = {};
     for (const target of Object.values(round.votes || {})) {
       counts[target] = (counts[target] || 0) + 1;
     }
 
-    let topId = null;
-    let topCount = 0;
-    let tied = false;
+    let topId = null, topCount = 0, tied = false;
     for (const [pid, c] of Object.entries(counts)) {
       if (c > topCount) { topCount = c; topId = pid; tied = false; }
       else if (c === topCount) { tied = true; }
@@ -38,9 +35,7 @@ module.exports = async (req, res) => {
       if (p._id === round.imposterId && imposterWon) delta = 3;
       if (p._id !== round.imposterId && imposterCaught) delta = 1;
       if (p._id !== round.imposterId && round.votes?.[p._id] === round.imposterId) delta += 1;
-      if (delta > 0) {
-        await db.collection("players").updateOne({ _id: p._id }, { $inc: { score: delta } });
-      }
+      if (delta > 0) await db.collection("players").updateOne({ _id: p._id }, { $inc: { score: delta } });
     }
 
     await db.collection("rounds").updateOne(
@@ -48,9 +43,9 @@ module.exports = async (req, res) => {
       { $set: { status: "results", votedOutId, imposterWon } }
     );
 
-    res.status(200).json({ ok: true, imposterWon, votedOutId });
+    return res.status(200).json({ ok: true, imposterWon, votedOutId });
   } catch (e) {
-    console.error(e);
+    console.error("tally error:", e);
     res.status(500).json({ error: e.message });
   }
 };
